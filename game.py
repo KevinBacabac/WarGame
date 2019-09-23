02e6e3b2ad431e2d655f5356249b81d6fe0a208d
Kevin <11673634+KevinBacabac@users.noreply.github.com>
2019-09-22 20:58:18 -0400

    Start game.py and resources

---
+++ game.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+from typing import List
+
+
+
+
+
+class Country:
+    DEFAULT_HEALTH = 100
+    DEFAULT_RESOURCES = 100
+
+    def __init__(self, player):
+        self.alive = True
+        self.health = self.DEFAULT_HEALTH
+        self.resources = self.DEFAULT_RESOURCES
+        self.player = player
+
+
+    def action(self):
+        # Check if action is valid
+        # If action is invalid, nuke own country
+        # Format action before appending
+        pass
+
+
+    def take_damage(self, damage):
+        self.health -= damage
+
+        if self.health < 0:
+            self.alive = False
+
+
+class Game:
+    MAX_TURNS = 100
+
+    def __init__(self, countries: List[Country]):
+        self.countries = countries
+        self.turn = 1
+
+
+    def start(self):
+        while (sum(country.alive for country in self.countries) > 1
+               and self.turn <= self.MAX_TURNS):
+            actions = self._get_actions()
+            self._run_actions(actions)
+
+
+
+    def _get_actions(self):
+        actions = []
+        for country in self.countries:
+            if not country.alive:
+                continue
+
+            action = country.action()
+
+
+
+            actions.append(action)
+
+        return actions
+
+    def _run_actions(self, actions):
+        pass
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ resources/weapons.py
@@ -0,0 +1,19 @@
+class Nuke:
+    COST = 100
+    DAMAGE = 100
+    SPEED = 3
+    REVEAL_CHANCE = 1
+
+
+class Missile:
+    COST = 10
+    DAMAGE = 20
+    SPEED = 3
+    REVEAL_CHANCE = 0.25
+
+
+class Laser:
+    COST = 10
+    DAMAGE = 20
+    SPEED = 1
+    REVEAL_CHANCE = 1
