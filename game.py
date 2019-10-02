c076e49074408ed7538982b12a4e549ddfeb239d
Kevin <11673634+KevinBacabac@users.noreply.github.com>
2019-10-01 23:54:29 -0400

    Finalize combat demo

--- game.py
+++ game.py
@@ -3,7 +3,7 @@
 from typing import List
 from bots import sample_bot
 from resources import weapons
-
+import time
 
 class Country:
     DEFAULT_HEALTH = 100
@@ -23,6 +23,7 @@ class Country:
         country_status = self.serialize()
 
         action = self.player.action(country_status, world_state)
+        action["ID"] = self.id
         return action
 
 
@@ -30,6 +31,7 @@ class Country:
         country_status = {
             "Alive": self.alive,
             "Health": self.health,
+            "ID": self.id,
             "Resources": self.resources,
             "Nukes": self.nukes
         }
@@ -40,15 +42,20 @@ class Country:
     def take_damage(self, damage):
         self.health -= damage
 
-        if self.health < 0:
-            self.alive = False
+        if self.health <= 0:
+            self.health = 0
+
+        else:
+            pass  # print(self.id, "HP is", self.health)
 
 
 class Game:
-    MAX_TURNS = 100
+    MAX_TURNS = 30
 
     def __init__(self, countries: List[Country]):
         self.countries = countries
+        self.events = []
+
         # Initialize ids
         for i, country in enumerate(self.countries):
             country.id = i
@@ -60,9 +67,18 @@ class Game:
         while (self._get_alive_count() > 1
                and self.turn <= self.MAX_TURNS):
 
+            print("Round", self.turn)
             actions = self._get_actions()
             self._run_actions(actions)
+            self._print_events()
             self.turn += 1
+
+        if self._get_alive_count() == 1:
+            alive = self._get_alive_countries()[0]
+            print(alive, "is the last one standing.")
+
+        else:
+            print("There were no survivors.")
 
 
     def _get_alive_count(self):
@@ -78,7 +94,8 @@ class Game:
     def _get_world_state(self):
         return {
             "countries": self._serialize_countries(),
-            "player_count": self._get_alive_count()
+            "events": self.events,
+            "alive_players": self._get_alive_countries()
         }
 
 
@@ -96,7 +113,7 @@ class Game:
             # If action is invalid, nuke own country
             if not self._is_valid_action(action):
                 action = {
-                    "Weapon": "Wait"
+                    "Action": "Wait"
                 }
 
             actions.append(action)
@@ -105,12 +122,14 @@ class Game:
 
 
     def _is_valid_action(self, action):
-        return all(
-            "Target" in action,
-            "Weapon" in action,
-            action["Weapon"] in weapons.Weapons,
-            action["Target"] in self._get_alive_countries()
-        )
+        try:
+            return all((
+                weapons.Weapons.has(action["Action"]),
+                action["Target"] in self._get_alive_countries()
+            ))
+        except KeyError:
+            print("KeyError", action)
+            return False
 
 
     def _serialize_countries(self):
@@ -121,27 +140,66 @@ class Game:
         return countries
 
     def _run_actions(self, actions):
+        self.events = []
+        alive = self._get_alive_countries()
+
         for action in actions:
-            if action["Weapon"] != "Wait":
-                continue
+            if action["Action"] == 0:
+                self.events.append(action)
 
-            if action["Weapon"] == 1:  # LASER
-                pass
+            elif action["Action"] == 1:  # LASER
+                self.events.append(action)
+                self.countries[action["Target"]].take_damage(20)
 
-            elif action["Weapon"] == 2:  # Missile
-                pass
+            elif action["Action"] == 2:  # Missile
+                self.events.append(action)
+                self.countries[action["Target"]].take_damage(20)
 
-            elif action["Weapon"] == 3:  # Nuke
-                pass
+            elif action["Action"] == 3:  # Nuke
+                action["Success"] = bool(self.countries[action["ID"]].nukes)
+                self.events.append(action)
 
-    def _nuke_country(self, target):
-        pass
+                if self.countries[action["ID"]].nukes:
+                    self.countries[action["ID"]].nukes -= 1
+
+                    self.countries[action["Target"]].take_damage(100)
+
+
+        # Kill players who died this turn
+        for player in alive:
+            if self.countries[player].health == 0:
+                self.countries[player].alive = False
+                self.events.append({
+                    "Action": "Death",
+                    "ID": player
+                })
+
+    def _print_events(self):
+        for event in self.events:
+            if event["Action"] == 0:
+                print(event["ID"], "decided to wait.")
+
+            elif event["Action"] == 1:  # LASER
+                print(event["ID"], "fired a laser at", event["Target"])
+
+            elif event["Action"] == 2:  # Missile
+                print(event["ID"], "fired a missile at", event["Target"])
+
+            elif event["Action"] == 3:
+                print(event["ID"], "fired a nuke at", event["Target"])
+                if not event["Success"]:
+                    print("But they ran out of nukes.")
+
+            elif event["Action"] == "Death":
+                print(event["ID"], "is dead!")
+
+        time.sleep(1)
 
 
 
 def main():
     countries = []
-    for _ in range(10):
+    for _ in range(6):
         countries.append(Country(sample_bot.Bot()))
 
     active_game = Game(countries)
